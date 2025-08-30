9444fd30033510b77670d0b4ca74ec83754cff7d
Elizaveta Grushnikova <46537176+grushnik@users.noreply.github.com>
2025-08-30 16:00:57 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Website/',   // <-- IMPORTANT for project pages
+  base: '/Website/', // <— MUST match your repo name
 })
