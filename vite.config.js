f70208000afd22b4e0ee77cf77e2051e38780d89
Elizaveta Grushnikova <46537176+grushnik@users.noreply.github.com>
2025-09-03 13:27:07 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: base must match your repo name for GitHub Pages project sites
 export default defineConfig({
-  base: '/N2bio/',
+  base: '/N2bio/',      // ⬅️ IMPORTANT: repo name
   plugins: [react()],
   build: { outDir: 'dist' }
 })
-
