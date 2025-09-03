8a969636b40338d75f0902d1a4178036d27b3f6a
Elizaveta Grushnikova <46537176+grushnik@users.noreply.github.com>
2025-09-03 13:20:06 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
+// src/main.jsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
-
+createRoot(document.getElementById('root')).render(<App />)
