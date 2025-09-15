0c808a202072a019303021119765948414b13c4d
Elizaveta Grushnikova <46537176+grushnik@users.noreply.github.com>
2025-09-15 13:09:09 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Plasma-Torch/',  // ✅ use new repo name
+  base: '/Plasma-Torch/',   // repo name from GitHub Pages
   plugins: [react()],
-  build: { outDir: 'dist' }
+  build: {
+    outDir: 'dist'
+  }
 })
-
